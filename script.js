0fd1f529f9e599ffabb6bf035d3e7f242b4f8f79
Artem Golubchikov <artem@golubchikov.ml>
2020-07-21 19:20:24 +0300

    created layout structure

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+function id(e) { return document.getElementById(e); }
+
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'Привет, Vue!'
+    title: 'BOOK YOUR BARBER',
+    header: 'Great Hair Doesn\'t Happen By Chance. It Happens By Appointment!<br />So Don\'t Wait And Book Your Appointment Now!',
+    formheader: 'BOOK YOUR APPOINTMENT',
+    mobileheader: 'Great Hair Doesn\'t Happen By Chance. It Happens By Appointment!',
+    success: 'APPOINTMENT SUCCESSFULLY BOOKED'    
   }
 });
 
